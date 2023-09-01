856edfb57a79f0a4b27d402c7958edab3c443765
ha6oon <hayun-lee@naver.com>
2023-09-01 13:20:11 +0900

    feat : Add file emr to redshift job

---
+++ dags/plugins/emr_elt_redshift.py
@@ -0,0 +1,285 @@
+from pyspark.sql import SQLContext, Window, SparkSession
+from pyspark.sql.functions import col, expr, round, \
+    avg, countDistinct, rank, split, posexplode, count, regexp_replace
+from pyspark.sql.window import Window
+#import sys, subprocess
+#subprocess.check_call([sys.executable, "-m", "pip", "install", "boto3"])
+import boto3
+import json
+
+def main():
+    secrets_manager = boto3.client(service_name='secretsmanager', region_name='us-west-2')
+    secret_json = secrets_manager.get_secret_value(SecretId='DE-2-1-SECRET')["SecretString"]
+    url = json.loads(secret_json)['redshift_conn_url']
+
+
+    # Initialize Spark session
+    spark = SparkSession \
+        .builder \
+        .appName("AWS REDSHIFT PYSPARK APP") \
+        .getOrCreate()
+
+    # Read data from Redshift
+    raw_data_schema = "RAW_DATA"
+    analytics_schema = "ANALYTICS"
+
+
+    # integrate for_ load
+    raw_data_table_names = ["brand", "media", "media_hashtag", "brand_log"]
+    read_dfs = {}
+    for read_table_name in raw_data_table_names:
+        df = spark.read \
+            .format("jdbc") \
+            .option("driver", "com.amazon.redshift.jdbc42.Driver") \
+            .option("url", url) \
+            .option("dbtable", f"{raw_data_schema}.{read_table_name}") \
+            .load()
+        read_dfs[read_table_name] = df
+
+    brand_df = read_dfs["brand"]
+    media_df = read_dfs["media"]
+    media_hashtag_df = read_dfs["media_hashtag"]
+    brand_log_df = read_dfs["brand_log"]
+
+    # !!!!!!!!!!!!!!elt!!!!!!!!!!!!!
+
+    # create brand_basic_info table
+    brand_basic_info_df = brand_df.select("tag_name", "profile_picture_url", "followers_count", "user_id")
+    # brand_basic_info_df.createOrReplaceTempView("brand_basic_info")
+    #brand_basic_info_df.show()
+
+    # create brand_information table
+    brand_information_df = media_df.join(
+        brand_df,
+        "user_id",
+        "left"
+    ).filter(
+        col("media_product_type").isin("FEED", "REELS")
+    ).select(
+        brand_df["tag_name"],
+        brand_df["user_id"],
+        brand_df["media_count"],
+        brand_df["followers_count"],
+        brand_df["updated_at"],
+        media_df["media_id"],
+        media_df["like_count"],
+        media_df["comments_count"],
+        media_df["ts"],
+        media_df["media_type"],
+        media_df["media_product_type"]
+    )
+
+    # Calculate derived columns and add aliases
+    brand_information_df = brand_information_df.withColumn(
+        "engagement",
+        media_df["like_count"] + media_df["comments_count"]
+    ).withColumn(
+        "engagement_rate",
+        round((col("engagement") / col("followers_count")) * 100, 2)
+    )
+    # brand_information_df.createOrReplaceTempView("brand_information")
+    # brand_information_df.show()
+
+    # Add columns
+    brand_information_df = brand_information_df.withColumn(
+        "post_time",
+        expr("EXTRACT(HOUR FROM ts)")
+    ).withColumn(
+        "day_of_week",
+        expr("EXTRACT(DAYOFWEEK FROM ts)")
+    ).withColumn(
+        "post_day_of_week",
+        expr("""
+            CASE EXTRACT(DAYOFWEEK FROM ts)
+                WHEN 1 THEN 'Sunday'
+                WHEN 2 THEN 'Monday'
+                WHEN 3 THEN 'Tuesday'
+                WHEN 4 THEN 'Wednesday'
+                WHEN 5 THEN 'Thursday'
+                WHEN 6 THEN 'Friday'
+                WHEN 7 THEN 'Saturday'
+            END AS post_day_of_week
+            """
+             )
+    )
+
+
+    # create followers_growth table
+    followers_growth_df = brand_log_df.join(
+        brand_df,
+        "user_id",
+        "left"
+    ).select(
+        brand_df["tag_name"],
+        brand_log_df["followers_count"],
+        brand_log_df["created_at"]
+    )
+    # followers_growth_df.createOrReplaceTempView("followers_growth")
+    # followers_growth_df.show()
+
+    # create hashtag_search table
+    hashtag_search_df = media_hashtag_df.join(
+        brand_df,
+        "user_id",
+        "left"
+    ).select(
+        brand_df["tag_name"],
+        media_hashtag_df["media_id"],
+        media_hashtag_df["caption"],
+        media_hashtag_df["ts"],
+        media_hashtag_df["user_id"],
+        media_hashtag_df["created_at"]
+    )
+    # hashtag_search_df.createOrReplaceTempView("hashtag_search")
+    # hashtag_search_df.show()
+
+    # create trending_topics table
+    trending_topics_df = hashtag_search_df.select(
+        col("ts"),
+        col("tag_name"),
+        col("created_at"),
+        posexplode(split(regexp_replace("caption", "#+", " #"), " ")).alias("pos", "related_hashtag")
+    ).filter(
+        col("related_hashtag").like("#_%")
+    )
+    # trending_topics_df.createOrReplaceTempView("trending_topics")
+
+    # create hashtag_count table
+    hashtag_count_df = trending_topics_df.groupBy("related_hashtag").agg(count("*").alias("hashtag_frequency"))
+    # hashtag_count_df.createOrReplaceTempView("hashtag_count_df")
+
+    # hashtag_count_df.show()
+
+    # 1000
+    hashtag_count_df = hashtag_count_df.orderBy(col("hashtag_frequency").desc()) \
+        .limit(1000)
+    #hashtag_count_df.show()
+
+    # create aggregated_brand_information table
+    aggregated_brand_information_df = brand_information_df.groupBy("tag_name").agg(
+        round(avg("engagement_rate"), 4).alias("avg_engagement_rate"),
+        countDistinct("media_id").alias("brand_media_cnt")
+    )
+    # aggregated_brand_information_df.createOrReplaceTempView("aggregated_brand_information")
+    #aggregated_brand_information_df.show()
+
+    # create aggregated_hashtag_search table
+    aggregated_hashtag_search_df = hashtag_search_df.groupBy("tag_name").agg(
+        countDistinct("media_id").alias("hashtaged_media_cnt")
+    )
+
+    # aggregated_hashtag_search_df.createOrReplaceTempView("aggregated_hashtag_search")
+    #aggregated_hashtag_search_df.show()
+
+    # create awareness table
+    # awareness_df = hashtag_search_df.select("tag_name", "created_at", "ts", "media_id")
+
+    # create popularity_factor_early_stage table
+    popularity_factor_early_stage_df = aggregated_brand_information_df.join(
+        aggregated_hashtag_search_df,
+        "tag_name",
+        "left"
+    ).select(
+        aggregated_brand_information_df["tag_name"],
+        aggregated_brand_information_df["avg_engagement_rate"],
+        aggregated_hashtag_search_df["hashtaged_media_cnt"]
+    )
+    # popularity_factor_early_stage_df.createOrReplaceTempView("popularity_factor_early_stage")
+    #popularity_factor_early_stage_df.show()
+
+    # create popularity_factor table
+    # Assuming popularity_factor_early_stage_df and brand_basic_info_df are your input DataFrames
+    popularity_factor_df = popularity_factor_early_stage_df.alias("pfe").join(
+        brand_basic_info_df.alias("bbi"),
+        "tag_name",
+        "left"
+    ).select(
+        col("pfe.tag_name"),
+        col("pfe.avg_engagement_rate"),
+        col("pfe.hashtaged_media_cnt"),
+        col("bbi.followers_count")
+    )
+
+    #popularity_factor_df.show()
+
+    # Add rank columns using the rank() function
+    popularity_factor_df = popularity_factor_df.withColumn(
+        "rank_avg_engagement_rate",
+        rank().over(Window.orderBy(col("avg_engagement_rate").desc()))
+    ).withColumn(
+        "rank_hashtaged_media_cnt",
+        rank().over(Window.orderBy(col("hashtaged_media_cnt").desc()))
+    ).withColumn(
+        "rank_followers_count",
+        rank().over(Window.orderBy(col("followers_count").desc()))
+    )
+    # popularity_factor_df.createOrReplaceTempView("popularity_factor")
+    #popularity_factor_df.show()
+
+    # create popularity_calculation table
+    popularity_calculation_df = popularity_factor_df.select(
+        col("tag_name"),
+        (col("rank_avg_engagement_rate") + col("rank_hashtaged_media_cnt") + col("rank_followers_count")).alias(
+            "sum_rank")
+    )
+    # Calculate the final popularity rank
+    popularity_calculation_df = popularity_calculation_df.withColumn(
+        "popularity_rank",
+        rank().over(Window.orderBy(col("sum_rank")))
+    )
+    # popularity_calculation_df.createOrReplaceTempView("popularity_calculation")
+    #popularity_calculation_df.show()
+
+    # create brand_media_post_time table
+    brand_media_post_time_df = brand_information_df.select(
+        col("ts"),
+        col("tag_name"),
+        expr("EXTRACT(HOUR FROM ts)").alias("post_time"),
+        expr("EXTRACT(DAYOFWEEK FROM ts)").alias("day_of_week"),
+        expr("""
+            CASE EXTRACT(DAYOFWEEK FROM ts)
+                WHEN 1 THEN 'Sunday'
+                WHEN 2 THEN 'Monday'
+                WHEN 3 THEN 'Tuesday'
+                WHEN 4 THEN 'Wednesday'
+                WHEN 5 THEN 'Thursday'
+                WHEN 6 THEN 'Friday'
+                WHEN 7 THEN 'Saturday'
+            END AS post_day_of_week
+        """).alias("post_day_of_week")
+    )
+    # brand_media_post_time_df.createOrReplaceTempView("brand_media_post_time")
+    #brand_media_post_time_df.show()
+
+    # Write transformed data to a new table in Redshift
+
+    # integrate for_ save
+    df_list_overwrite = [
+        (brand_basic_info_df, "brand_basic_info"),
+        (brand_information_df, "brand_information"),
+        (followers_growth_df, "followers_growth"),
+        (aggregated_brand_information_df, "aggregated_brand_information"),
+        (aggregated_hashtag_search_df, "aggregated_hashtag_search"),
+        (popularity_factor_df, "popularity_factor"),
+        (popularity_calculation_df, "popularity_calculation"),
+        (brand_media_post_time_df, "brand_media_post_time"),
+        (hashtag_count_df, "hashtag_count")
+    ]
+
+    # !!!!!!!!!!!overwriting!!!!!!!!!!!!
+    for write_df, write_table_name in df_list_overwrite:
+        write_df.write \
+                .format("jdbc") \
+                .option("driver", "com.amazon.redshift.jdbc42.Driver") \
+                .option("url", url) \
+                .option("dbtable", f"{analytics_schema}.{write_table_name}") \
+                .mode("append") \
+                .save()
+
+
+    spark.stop()
+    print("Finish")
+
+
+if __name__ == "__main__":
+    main()
