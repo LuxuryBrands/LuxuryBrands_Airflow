cc8580b426dcf20f8f53889cb82bed47b24fa3fe
MACLSH <gracia10@daum.net>
2023-09-03 00:41:27 +0900

    fix : Fix param name
    - 함수 참조하는 이름 변경

--- dags/elt_dag.py
+++ dags/elt_dag.py
@@ -12,8 +12,6 @@ from airflow.providers.amazon.aws.operators.emr import EmrAddStepsOperator
 from airflow.providers.amazon.aws.sensors.emr import EmrStepSensor
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 
-
-
 default_args = {
     'start_date': days_ago(1),
     'retries': 3,
@@ -23,17 +21,16 @@ default_args = {
 }
 
 dag = DAG(
-    'emr_to_redshift',
+    'elt_emr_to_redshift',
     default_args=default_args,
     schedule_interval='30 * * * *',  # Run the DAG every 30 minutes past the hour
     catchup=False,  # Do not catch up on historical runs
 )
 
-script_key=f"emr/scripts/emr_elt_redshift.py"
+script_key = f"emr/scripts/emr_elt_redshift.py"
 bucket_name = "de-2-1-s3"
 spark_steps = get_elt_step(bucket_name, script_key)
 job_flow_name = 'de-2-1-emr'
-
 
 default_args = {
     'start_date': days_ago(1),
@@ -43,63 +40,60 @@ default_args = {
     'on_success_callback': slack.send_success_alert
 }
 
-
-
 redshift_truncate_table_task1 = PostgresOperator(
     task_id='setup_create_table1',
     sql='TRUNCATE TABLE analytics.brand_basic_info;',
     postgres_conn_id="REDSHIFT_DEFAULT",
-    dag = dag
+    dag=dag
 )
 redshift_truncate_table_task2 = PostgresOperator(
     task_id='setup_create_table2',
     sql='TRUNCATE TABLE analytics.brand_information;',
     postgres_conn_id="REDSHIFT_DEFAULT",
-    dag = dag
+    dag=dag
 )
 redshift_truncate_table_task3 = PostgresOperator(
     task_id='setup_create_table3',
     sql='TRUNCATE TABLE analytics.aggregated_brand_information; ',
     postgres_conn_id="REDSHIFT_DEFAULT",
-    dag = dag
+    dag=dag
 )
 redshift_truncate_table_task4 = PostgresOperator(
     task_id='setup_create_table4',
     sql='TRUNCATE TABLE analytics.brand_media_post_time;',
     postgres_conn_id="REDSHIFT_DEFAULT",
-    dag = dag
+    dag=dag
 )
 redshift_truncate_table_task5 = PostgresOperator(
     task_id='setup_create_table5',
     sql='TRUNCATE TABLE analytics.followers_growth; ',
     postgres_conn_id="REDSHIFT_DEFAULT",
-    dag = dag
+    dag=dag
 )
 redshift_truncate_table_task6 = PostgresOperator(
     task_id='setup_create_table6',
     sql='TRUNCATE TABLE analytics.aggregated_hashtag_search;',
     postgres_conn_id="REDSHIFT_DEFAULT",
-    dag = dag
+    dag=dag
 )
 redshift_truncate_table_task7 = PostgresOperator(
     task_id='setup_create_table7',
     sql='TRUNCATE TABLE analytics.popularity_factor; ',
     postgres_conn_id="REDSHIFT_DEFAULT",
-    dag = dag
+    dag=dag
 )
 redshift_truncate_table_task8 = PostgresOperator(
     task_id='setup_create_table8',
     sql='TRUNCATE TABLE analytics.popularity_calculation;',
     postgres_conn_id="REDSHIFT_DEFAULT",
-    dag = dag
+    dag=dag
 )
 redshift_truncate_table_task9 = PostgresOperator(
     task_id='setup_create_table9',
     sql='TRUNCATE TABLE analytics.hashtag_count;',
     postgres_conn_id="REDSHIFT_DEFAULT",
-    dag = dag
+    dag=dag
 )
-
 
 # spark_job
 get_cluster_id = PythonOperator(
@@ -112,7 +106,7 @@ get_cluster_id = PythonOperator(
 script_to_s3 = PythonOperator(
     task_id="script_to_s3",
     python_callable=upload_to_s3,
-    op_kwargs={"filename": f"./dags/plugins/emr_elt_redshift.py", "key": script_key, "bucket_name": bucket_name},
+    op_kwargs={"files": ["./dags/plugins/emr_elt_redshift.py"], "keys": [script_key], "bucket_name": bucket_name},
     dag=dag
 )
 
@@ -131,4 +125,4 @@ step_checker = EmrStepSensor(
             + "] }}",
     dag=dag
 )
-redshift_truncate_table_task1 >> redshift_truncate_table_task2 >> redshift_truncate_table_task3 >> redshift_truncate_table_task4 >>  redshift_truncate_table_task5 >> redshift_truncate_table_task6 >> redshift_truncate_table_task7 >> redshift_truncate_table_task8 >> redshift_truncate_table_task9 >> script_to_s3 >> add_steps >> step_checker
+redshift_truncate_table_task1 >> redshift_truncate_table_task2 >> redshift_truncate_table_task3 >> redshift_truncate_table_task4 >> redshift_truncate_table_task5 >> redshift_truncate_table_task6 >> redshift_truncate_table_task7 >> redshift_truncate_table_task8 >> redshift_truncate_table_task9 >> script_to_s3 >> add_steps >> step_checker
